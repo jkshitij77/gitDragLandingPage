584fd2fbbd621f1fbb2af676b4577349cee83780
Kshitij Jain <Kshitij.jain.23@dartmouth.edu>
2022-04-11 21:08:04 -0400

    Almost done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+// const $ = require('jquery');
+
+let num = 0;
+
+setInterval(() => {
+  num += 1;
+  // console.log(`hello ${num}`);
+  $('#main').html(`You have been here for ${num} seconds`);
+}, 1000);
