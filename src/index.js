c9f1149aaca2e6f632e1a5b9b4b3f5babeec6fb8
Kshitij Jain <Kshitij.jain.23@dartmouth.edu>
2022-04-15 00:17:13 -0400

    Added React to starter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import $ from 'jquery';
+// import $ from 'jquery';
+// import './style.scss';
+// // const $ = require('jquery');
+
+// let num = 0;
+
+// setInterval(() => {
+//   num += 1;
+//   // console.log(`hello ${num}`);
+//   $('#main').html(`You have been here for ${num} seconds`);
+// }, 1000);
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import './style.scss';
-// const $ = require('jquery');
 
-let num = 0;
+// function App() {
+//   return <div className="test">All the REACT are belong to us!</div>;
+// }
 
-setInterval(() => {
-  num += 1;
-  // console.log(`hello ${num}`);
-  $('#main').html(`You have been here for ${num} seconds`);
-}, 1000);
+function App() {
+  return <div className="test">All the REACT are belong to us!</div>;
+}
+
+const root = createRoot(document.getElementById('main'));
+root.render(<App />);
