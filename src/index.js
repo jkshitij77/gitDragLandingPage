5abc18b346f3407819bf2d760152f4ab37917533
Kshitij Jain <Kshitij.jain.23@dartmouth.edu>
2022-04-27 00:37:25 -0400

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,70 +1,24 @@
-// import $ from 'jquery';
-// import './style.scss';
-// // const $ = require('jquery');
-
-// let num = 0;
-
-// setInterval(() => {
-//   num += 1;
-//   // console.log(`hello ${num}`);
-//   $('#main').html(`You have been here for ${num} seconds`);
-// }, 1000);
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './style.scss';
-import {
-  BrowserRouter, Routes, Route, NavLink, useParams,
-} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
+import Counter from './components/counter';
+import App from './components/app';
+import Controls from './components/control';
 
-// function App() {
-//   return <div className="test">All the REACT are belong to us!</div>;
-// }
-function About(props) {
-  return <div> All there is to know about me </div>;
-}
-function Welcome(props) {
-  return <div>Welcome</div>;
-}
-function Nav(props) {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/" exact>Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
-      </ul>
-    </nav>
-  );
-}
-function Test(props) {
-  const { id } = useParams();
-  return <div> ID: {id} </div>;
-}
-function FallBack(props) {
-  return <div>URL Not Found</div>;
-}
-
-function App(props) {
-  return (
-    <BrowserRouter>
-      <div>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/test/:id" element={<Test />} />
-          <Route path="*" element={<FallBack />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-}
-
-// function App() {
-//   return <div className="test">All the REACT are belong to us!</div>;
-// }
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+// boilerplate to copy, don't have to know
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+));
 
 const root = createRoot(document.getElementById('main'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+    <Counter />
+    <Controls />
+  </Provider>,
+);
